4c4bbf58bd82cbd6057574e51730d5c4630f6eaa
BrySayan <bryansalfattifernandez@gmail.com>
2024-07-19 21:03:35 -0400

    Commit 2.1

--- acumulativa/src/components/Posts.jsx
+++ acumulativa/src/components/Posts.jsx
@@ -6,7 +6,7 @@ export function Posts() {
     const tituloRef = useRef();
     const descripcionRef = useRef();
     const [mensaje, setMensaje] = useState('');
-    const [postList, setPostList] = useState([]);
+    const [listaPost, setListaPost] = useState([]);
 
     function añadirPost() {
         const titulo = tituloRef.current.value;
@@ -26,9 +26,32 @@ export function Posts() {
             description: descripcion
         };
 
-        setListaPost([...postList, nuevoPost])
+        setListaPost([...listaPost, nuevoPost])
 
         tituloRef.current.value = '';
         descripcionRef.current.value = '';
     }
+
+
+    function eliminarPost(id) {
+        const actualizarPosts = listaPost.filter(post => post.id !== id);
+        setListaPost(actualizarPosts)
+    }
+
+    return (
+        <div ClassName="container">
+            <h1>Notas</h1>
+            <div ClassName="d-flex">
+                <input ref={tituloRef} className="form-control m-2" type="text" placeholder="Titulo" />
+                <input ref={descripcionRef} className="form-control m-2" type="text" placeholder="Descripcion"/>
+                <button className="btn btn-success m2" onClick={añadirPost}>Agregar Post</button>
+            </div>
+            <div className="alert alert-danger mt2" role="alert" hidden={!mensaje}>
+                {mensaje}
+            </div>
+            <div className="d-flex flex-wrap">
+                {listaPost.map(Post => <Post post={post} key={post.id} removePost={eliminarPost} />)}
+            </div>
+        </div>
+    );
 }
